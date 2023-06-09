c27291f866f786414853ad88cda441d0f22221eb
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-06-09 11:57:36 +0200

    Added unit tests for getDefaultPermittedPackages.R

---
+++ tests/testthat/setup.R
@@ -0,0 +1,14 @@
+library(PaRe)
+library(git2r)
+library(testthat)
+
+tmpDir <- tempdir()
+pathToRepo <- file.path(tmpDir, "glue")
+
+clone(
+  url = "https://github.com/tidyverse/glue",
+  local_path = pathToRepo,
+  credentials = cred_token()
+)
+
+repo <- Repository$new(path = pathToRepo)
