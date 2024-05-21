22b9244a5a1af2107a1a504e0f918990bf81c9b3
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-05-21 13:33:17 +0200

    Skip on 4.5.0

--- tests/testthat/test-checkDependencies.R
+++ tests/testthat/test-checkDependencies.R
@@ -15,6 +15,9 @@ test_that("void", {
 })
 
 test_that("minimal", {
+  skip_if(
+    utils::packageVersion(pkg = "base") <= package_version("4.4.0")
+  )
   repo <- makeRepo()
   testthat::skip_if(!R6::is.R6(repo))
 
@@ -22,5 +25,5 @@ test_that("minimal", {
     checkDependencies(repo),
     "All dependencies are approved."
   )
-  #unlink(repo$getPath(), recursive = TRUE)
+  unlink(repo$getPath(), recursive = TRUE)
 })
