977b92bbf1ab638b897a363afc70a609df7e1a39
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-11-01 10:17:10 +0100

    Updated tests

--- tests/testthat/test-getFunCall.R
+++ tests/testthat/test-getFunCall.R
@@ -1,9 +1,20 @@
 test_that("minimal", {
   repo <- makeRepo()
-  testthat::skip_if(!R6::is.R6(repo))
-  file <- repo$getRFiles()[[2]]
-  fun <- file$getFunctions()[[2]]
+  testthat::skip_if_not(all(class(repo) == c("Repository", "R6")))
+  files <- repo$getRFiles()
+  glueIdx <- sapply(files, function(file) {
+    file$getName() == "glue.R"
+  })
+
+  file <- files[glueIdx][[1]]
+  funs <- file$getFunctions()
   defFuns <- PaRe::getDefinedFunctions(repo)
+
+  glueIdx <- sapply(funs, function(fun) {
+    fun$getName() == "glue"
+  })
+
+  fun <- funs[glueIdx][[1]]
 
   df <- bind_rows(PaRe:::getFunCall(fun, defFuns))
   expect_true(nrow(df) == 4)
